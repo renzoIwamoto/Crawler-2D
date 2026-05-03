a66b9d6679209d385be4e136aa83d590f87d0e3e
renzoiwamoto-k <renzo.iwamoto@pucp.edu.pe>
2026-05-03 18:02:25 -0500

    crawler env init

--- src/envs/crawler_env.py
+++ src/envs/crawler_env.py
@@ -58,3 +58,73 @@ class CrawlerEnv:
         pygame.init()
         self.screen = pygame.display.set_mode((1280, 720))
         self.clock = pygame.time.Clock()
+
+    def reset(self):
+        # Torso
+        self.trunk.x = 200.0
+        self.trunk.y = 100.0
+        self.trunk.vx = 0.0
+        self.trunk.vy = 0.0
+        self.trunk.angle = 0.0
+        self.trunk.angular_velocity = 0.0
+
+        # Muslo izquierdo
+        self.thigh_L.x = self.trunk.x - TORSO_W/4
+        self.thigh_L.y = self.trunk.y + TORSO_H/2 + MUSLO_H/2
+        self.thigh_L.vx = 0.0
+        self.thigh_L.vy = 0.0
+        self.thigh_L.angle = 0.0
+        self.thigh_L.angular_velocity = 0.0
+
+        # Muslo derecho
+        self.thigh_R.x = self.trunk.x + TORSO_W/4
+        self.thigh_R.y = self.trunk.y + TORSO_H/2 + MUSLO_H/2
+        self.thigh_R.vx = 0.0
+        self.thigh_R.vy = 0.0
+        self.thigh_R.angle = 0.0
+        self.thigh_R.angular_velocity = 0.0
+
+        # Tibia izquierda
+        self.shin_L.x = self.trunk.x - TORSO_W/4
+        self.shin_L.y = self.thigh_L.y + MUSLO_H/2 + TIBIA_H/2
+        self.shin_L.vx = 0.0
+        self.shin_L.vy = 0.0
+        self.shin_L.angle = 0.0
+        self.shin_L.angular_velocity = 0.0
+
+        # Tibia derecha
+        self.shin_R.x = self.trunk.x + TORSO_W/4
+        self.shin_R.y = self.thigh_R.y + MUSLO_H/2 + TIBIA_H/2
+        self.shin_R.vx = 0.0
+        self.shin_R.vy = 0.0
+        self.shin_R.angle = 0.0
+        self.shin_R.angular_velocity = 0.0
+
+        # Joints
+        for joint in [self.hip_L, self.knee_L, self.hip_R, self.knee_R]:
+            joint.angle = 0.0
+            joint.angular_velocity = 0.0
+            joint.torque_input = 0.0
+
+        self.step_count = 0
+        return self._get_observation()
+
+    def _get_observation(self):
+        contact_L = 1.0 if self.shin_L.y + np.cos(self.knee_L.angle) * TIBIA_H/2 >= self.ground_y else 0.0
+        contact_R = 1.0 if self.shin_R.y + np.cos(self.knee_R.angle) * TIBIA_H/2 >= self.ground_y else 0.0
+
+        return np.array([
+            self.trunk.vx,
+            self.trunk.vy,
+            self.ground_y - self.trunk.y,
+            self.hip_L.angle,
+            self.hip_L.angular_velocity,
+            self.knee_L.angle,
+            self.knee_L.angular_velocity,
+            self.hip_R.angle,
+            self.hip_R.angular_velocity,
+            self.knee_R.angle,
+            self.knee_R.angular_velocity,
+            contact_L,
+            contact_R,
+        ], dtype=np.float32)
